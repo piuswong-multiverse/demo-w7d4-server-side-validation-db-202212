f3b05a4a192ca02d69986ee1500b16c20924084f
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-12 11:51:22 -0600

    Create PUT request endpoint for books

--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,21 @@ const router = express.Router();
 const {Books} = require('../models/index');
 
 // create endpoints;
+// GET
 router.get('/', async (req, res) => {
     // get all the books and send it out!
     res.send( await Books.findAll());
 })
+// POST
+// PUT
+router.put('/:id', async (req, res) => {
+    // modify one record in the db
+    const newBookData = req.body; // getting the body data (books)
+    const bookToModify = await Books.findByPk(req.params.id);
+    await bookToModify.update(newBookData);
+    // send out a response
+    res.json( await Books.findAll() );
+});
+// DELETE
 
 module.exports = router;
